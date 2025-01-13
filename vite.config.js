a464ee8d1b4cad7850718b886acb6b967d0b919e
mppdevv <mppdevv@skiff.com>
2025-01-13 20:00:52 +0000

    functionality

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite"
 
 export default defineConfig({
-    base: "/",
+    base: "/christmas-game/",
     build: {
         target: "esnext",
         rollupOptions: {
